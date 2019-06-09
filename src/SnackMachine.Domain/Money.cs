561b963df412b1be80ece294668bea864e618754
Emil Craciun <emil.craciun@live.com>
2019-06-09 11:43:33 +0300

    More money tests, refactoring, and new functionalities

--- src/SnackMachine.Domain/Money.cs
+++ src/SnackMachine.Domain/Money.cs
@@ -6,12 +6,20 @@ namespace SnackMachine.Domain
 {
     public class Money : ValueObject<Money>
     {
-        public int OneCentCount { get; set; }
-        public int TenCentCount { get; set; }
-        public int QuarterCount { get;  set; }
-        public int OneDollarCount { get;  set; }
-        public int FiveDollarCount { get;  set; }
-        public int TwentyDollarCount { get;  set; }
+        public int OneCentCount { get; }
+        public int TenCentCount { get; }
+        public int QuarterCount { get; }
+        public int OneDollarCount { get; }
+        public int FiveDollarCount { get; }
+        public int TwentyDollarCount { get; }
+
+        public decimal Amount =>
+            OneCentCount * 0.01m +
+            TenCentCount * 0.10m +
+            QuarterCount * 0.25m +
+            OneDollarCount +
+            FiveDollarCount * 5 +
+            TwentyDollarCount * 20;
 
         public Money(
             int oneCentCount,
@@ -53,6 +61,17 @@ namespace SnackMachine.Domain
                 money1.TwentyDollarCount + money2.TwentyDollarCount);
 
             return sum;
+        }
+
+        public static Money operator -(Money money1, Money money2)
+        {
+            return new Money(
+                money1.OneCentCount - money2.OneCentCount,
+                money1.TenCentCount - money2.TenCentCount,
+                money1.QuarterCount - money2.QuarterCount,
+                money1.OneDollarCount - money2.OneDollarCount,
+                money1.FiveDollarCount - money2.FiveDollarCount,
+                money1.TwentyDollarCount - money2.TwentyDollarCount);
         }
 
         protected override IEnumerable<object> GetEqualityComponents()
--- tests/SnackMachine.Domain.Tests/MoneyTests.cs
+++ tests/SnackMachine.Domain.Tests/MoneyTests.cs
@@ -67,5 +67,65 @@ namespace SnackMachine.Domain.Tests
 
             Assert.Throws<InvalidOperationException>(action);
         }
+
+        [Theory]
+        [InlineData(0, 0, 0, 0, 0, 0, 0)]
+        [InlineData(1, 0, 0, 0, 0, 0, 0.01)]
+        [InlineData(1, 2, 0, 0, 0, 0, 0.21)]
+        [InlineData(1, 2, 3, 0, 0, 0, 0.96)]
+        [InlineData(1, 2, 3, 4, 0, 0, 4.96)]
+        [InlineData(1, 2, 3, 4, 5, 0, 29.96)]
+        [InlineData(1, 2, 3, 4, 5, 6, 149.96)]
+        [InlineData(11, 0, 0, 0, 0, 0, 0.11)]
+        [InlineData(110, 0, 0, 0, 100, 0, 501.1)]
+        public void AmountIsCalculatedCorrectly(
+            int oneCentCount,
+            int tenCentCount,
+            int quarterCount,
+            int oneDollarCount,
+            int fiveDollarCount,
+            int twentyDollarCount,
+            decimal expectedAmount)
+        {
+            Money money = new Money(
+                oneCentCount,
+                tenCentCount,
+                quarterCount,
+                oneDollarCount,
+                fiveDollarCount,
+                twentyDollarCount);
+
+            money.Amount.Should().Be(expectedAmount);
+        }
+
+        [Fact]
+        public void SubtractionOfTwoMoneysProducesCorrectResult()
+        {
+            Money money1 = new Money(10, 10, 10, 10, 10, 10);
+            Money money2 = new Money(1, 2, 3, 4, 5, 6);
+
+            Money result = money1 - money2;
+
+            result.OneCentCount.Should().Be(9);
+            result.TenCentCount.Should().Be(8);
+            result.QuarterCount.Should().Be(7);
+            result.OneDollarCount.Should().Be(6);
+            result.FiveDollarCount.Should().Be(5);
+            result.TwentyDollarCount.Should().Be(4);
+        }
+
+        [Fact]
+        public void CannotSubtractMoreThanExists()
+        {
+            Money money1 = new Money(0, 1, 0, 0, 0, 0);
+            Money money2 = new Money(1, 0, 0, 0, 0, 0);
+
+            Action action = () =>
+            {
+                Money money = money1 - money2;
+            };
+
+            Assert.Throws<InvalidOperationException>(action);
+        }
     }
 }
